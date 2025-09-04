7b22a6851aa38de73a535957a1914ab7a7412fbe
sjsusu <serena.shirlin@gmail.com>
2025-09-03 20:38:50 -0500

    Switch to Using Decimals for Fractions
    This commit updates the code to use Decimal objects for fractions
    instead of floats. The switch to Decimal objects helps to avoid issues
    with floating-point precision that terminated binary conversion early.
    
    The following changes were also made:
    - Updated checking and order for corner cases
    - Updated docstrings for functions and switched to numpy style

--- assignment_1/assignment_1.py
+++ assignment_1/assignment_1.py
@@ -1,4 +1,5 @@
 import math
+from decimal import *
 
 """
 MATH 5334 - Assignment 1: Binary Encoding
@@ -24,33 +25,37 @@ and more...
 
 def decimal_to_binary(num_string):
     """
-    This function takes a decimal (string) and returns the number in 64-bit binary form (string). The returned string has spaces to indicate the sign, exponent, and fraction.
+    This function takes a decimal (string) and returns the number in IEEE 754 64-bit binary form (string). The returned string has spaces to indicate the sign, exponent, and fraction.
 
-    Parameter:
-        num_string (string) : user input
+    Parameters
+    ----------
+    num_string: string
+        The decimal provided from user input.
 
-    Return:
-        string : num_string in IEEE 754 64-bit binary. Formatted as "sign exponent fraction".
+    Returns
+    -------
+    string:
+        num_string converted to IEEE 754 64-bit binary. Formatted as "sign exponent fraction".
+
+    Raises
+    ------
+    TypeError
+        The input is not a decimal.
+    ValueError
+        The input is too small or large for IEEE 754 64-bit conversion.
     """
 
-    # Checks if user input is a valid decimal number within IEEE-754 64 bit binary.
+    # Check if user input is a decimal number
     try:
-        num = float(num_string)
+        num = Decimal(num_string)
+        num_abs = num.copy_abs()
+    except InvalidOperation:
+        raise TypeError("Input is not a decimal number, try again.")
 
-    # Not a valid decimal input
-    except ValueError:
-        raise ValueError("Input is not a decimal number, try again.")
-
-    # Overflow
-    except OverflowError:
-        raise OverflowError("Overflow error. Input is too large, try again.")
-
-    # TODO: underflow?
-
-    # Assigns the sign by looking for '-' from original input.
+    # Assign the sign by looking for '-' from original input.
     sign = "1" if num_string.strip()[0] == "-" else "0"
 
-    # Checks Cases for different types of floats
+    # Check cases for different types of floats
     # 1) Infinity (+ or -)
     if math.isinf(num):
         exponent = "".join(["1" for i in range(11)])
@@ -64,20 +69,29 @@ def decimal_to_binary(num_string):
         return " ".join([sign, exponent, fraction])
 
     # 3) Zero (+ or -)
-    elif num == 0:
+    elif num_abs == Decimal("0"):
         exponent = "".join(["0" for i in range(11)])
         fraction = "".join(["0" for i in range(52)])
         return " ".join([sign, exponent, fraction])
 
+    # Check Magnitude
+    # Too small
+    elif num_abs < Decimal('1e-308'):
+        raise ValueError("Input is too small, try again.")
+    # Too large
+    elif num_abs > Decimal('1e308'):
+        raise ValueError("Input is too large, try again.")
+
     # 4) General Case
     else:
         # Retrieve integer and fractional parts of number
-        fraction, integer = math.modf(num)
+        integer = num_abs.quantize(Decimal("1"), rounding=ROUND_FLOOR)
+        fraction = num_abs - integer
 
         # Convert integer portion to binary
-        integer_binary = integer_to_binary(integer)
+        integer_binary = integer_to_binary(float(integer))
 
-        # Convert fraction to binary
+        # Convert fraction to binary and update integer binary if rounded
         fraction_binary, integer_binary = fraction_to_binary(fraction, integer_binary)
 
         # Find exponent
@@ -111,14 +125,21 @@ def decimal_to_binary(num_string):
 # Helper functions
 def integer_to_binary(integer):
     """
-    This function takes and integer and returns its binary representation as a list of chars.
+    This function takes an integer and returns its binary representation as a list of chars.
+
     Returns empty list if integer entered is any form of 0.
-    Parameters:
-        integer: int
-    Returns:
-        list (string): The binary representation of integer as ordered list. Each index contains a '0' or '1'.
+
+    Parameters
+    ----------
+    integer: int or float
+        The integer to be converted into binary.
+
+    Returns
+    -------
+    list: string
+        The binary representation of integer as ordered list. Each index contains a '0' or '1'.
     """
-    dividend = math.fabs(integer)
+    dividend = integer
     integer_binary = []
     while True:
         quotient = dividend / 2
@@ -142,91 +163,124 @@ def fraction_to_binary(fraction, integer_binary):
     """
     This function takes a fractional portion of a number (as a decimal) and returns a tuple of
     (1) its binary representation as a list of chars and
-    (2) integer_binary (copy or rounded)
+    (2) integer_binary (copy or rounded).
 
     The length of list (1) is based on the number of bits remaining in the final mantissa.
-    Parameters:
-        fraction: float
-        integer_binary: list(string):
-            The binary representation of integer portion of number as list of chars '0' and '1'.
-            Used to calculated remaining bits (significant figures) in mantissa (before normalization).
-    Returns:
-        tuple: (fraction_binary, integer_binary_rounded)
 
-            fraction_binary:
-            list(string): The binary representation of fraction as ordered list. Each index contains a '0' or '1'.
+    Parameters
+    ----------
+    fraction: Decimal object
+        Fraction portion of number to be converted into binary.
+    integer_binary: list of string
+        The binary representation of integer portion of number as list of chars '0' and '1'.
+        Used to calculated remaining bits (significant figures) in mantissa (before normalization).
 
-            integer_binary_rounded:
-            list(string): If rounding occurs in the integers, a rounded version of integer_binary is provided. Otherwise, a copy of the original integer_binary is given.
+    Returns
+    -------
+    tuple of (list of string, list of string)
+        -  1st list of string:
+
+            The binary representation of fraction as ordered list. Each index contains a '0' or '1'.
+
+        - 2nd list of string:
+
+            If rounding occurs in the integers, a rounded version of integer_binary is provided.
+            Otherwise, a copy of the original integer_binary is given.
     """
 
-    TOLERANCE = 1e-14
+    TOLERANCE = Decimal("1e-14")
+    MAX_SIG_FIGS = 54
 
     # Calculates current significant figures
     sig_figs = len(integer_binary)
     fraction_binary = []
-    dividend = math.fabs(fraction)
+    dividend = fraction
 
     # Convert fraction to binary
-    while sig_figs < 54:
-        dividend *= 2
-        remainder = math.floor(dividend) % 2
+    while sig_figs < MAX_SIG_FIGS:
+        dividend *= Decimal("2")
+        remainder = dividend % Decimal("2")
+        remainder = remainder.quantize(Decimal("1"), rounding=ROUND_FLOOR)
         dividend -= remainder
+        fraction_binary.append(str(remainder))
 
         if dividend <= TOLERANCE:
             break
         elif (sig_figs == 0 and remainder == 1) or (sig_figs != 0):
             sig_figs += 1
 
-        fraction_binary.append(str(remainder))
-
     # Non-Rounding Cases
-    if sig_figs <= 53:
+    # 1) Early Truncation
+    if sig_figs <= (MAX_SIG_FIGS - 1):
         # Adds extra zeros if truncated early
         fraction_binary += (53 - sig_figs) * ["0"]
         return (fraction_binary, integer_binary)
 
-    elif fraction_binary[53] == "0":
+    # 2) Rounding is not needed (last digit is 0)
+    elif fraction_binary[len(fraction_binary) - 1] == "0":
         fraction_binary.pop()
         return (fraction_binary, integer_binary)
 
-    # Rounding Case
+    # Rounding Case (last digit is 1)
     else:
-        fraction_binary, rounding_integers = round_up(fraction_binary)
-        if rounding_integers:
+        fraction_binary.pop()
+        fraction_binary, rounding_integer = round_up(fraction_binary)
+        if rounding_integer:
             integer_binary_rounded, appending_integer = round_up(integer_binary)
 
             if appending_integer:
                 integer_binary_rounded.insert(0, "1")
 
-        return (fraction_binary, integer_binary_rounded)
+            integer_binary = integer_binary_rounded
+
+        return (fraction_binary, integer_binary)
 
 
 def round_up(binary):
     """
     Built as a helper to fraction_to_binary().
 
-    ASSUMES that a binary number will be rounded up. This function rounds up the given binary number starting with the last number WITHOUT PREPENDING. Returns a tuple of (1) rounded binary number (2) boolean indicating if '1' will need to appended to beginning.
-    Parameters:
-        binary: list(string): Binary representation of a number as ordered list of chars '0' and '1'.
+    ASSUMES that a binary number will be rounded up. This function rounds up the given binary number starting with the last number WITHOUT PREPENDING.
 
-            THE FOLLOWING ASSUMPTIONS ARE MADE:
-            If the number is a(n):
-                (i) INTEGER: '1' to be rounded remains in the binary representation as the last digit.
-                (ii) FRACTION: The last digit '1' to be rounded is removed from binary representation.
-    Returns:
-        tuple: (rounded_binary, prepend)
+    Returns a tuple of:
+    - rounded binary number
+    - boolean indicating if '1' will need to appended to beginning.
 
-        rounded_binary: list(string): The rounded binary number as an ordered list of chars '0' and '1'.
+    Parameters
+    ----------
+    binary: list of string
+        Binary representation of a number as ordered list of chars '0' and '1'.
 
-        prepend: bool: Indicates if a '1' needs to be appended to the beginning of rounded_binary.
+        THE FOLLOWING ASSUMPTIONS ARE MADE:
+
+        If the number is a(n):
+            - Integer:
+
+                '1' to be rounded remains in the binary representation as the last digit.
+
+            - Fraction:
+
+                The last digit '1' to be rounded is removed from binary representation.
+
+    Returns
+    -------
+    tuple of (list of string, bool)
+        - list of string:
+
+            The rounded binary number as an ordered list of chars '0' and '1'
+
+        - bool:
+
+            Indicates if a '1' needs to be appended to the beginning of from rounding.
     """
 
     length = len(binary)
 
+    # Case if integer is 0
     if length == 0:
         return (binary, True)
 
+    # General Case
     for i in range(length - 1, -1, -1):
         if binary[i] == "0":
             binary[i] = "1"
@@ -242,11 +296,6 @@ def round_up(binary):
 if __name__ == "__main__":
     line = "".join(["-" for i in range(74)])
     print(line, "Assignment 1: Binary Encoding", line, sep="\n")
-    print(
-        "Remark: Tail end of mantissa may not be fully \naccurate due to machine error.",
-        line,
-        sep="\n",
-    )
 
     while True:
         print("Note: Enter q to quit.", sep="\n")
