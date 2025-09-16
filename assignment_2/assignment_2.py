a6dcce75692237036b6c4f8c6a74864b267b56d0
sjsusu <serena.shirlin@gmail.com>
2025-09-15 22:37:07 -0500

    Add Figures Class and Equation Creation
    This commit introduces a `Figure` class to streamline the process of
    generating and saving plots. Plotting function calls have been
    refactored in the main script to utilize this new class.
    
    Additionally, a function to generate the Lagrange polynomial in LaTeX
    format has been implemented. Generated equations are written to txt
    files in the main method.

--- assignment_2/assignment_2.py
+++ assignment_2/assignment_2.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from figures import Figure
 
 
 def f(x):
@@ -40,98 +41,133 @@ def calculate_lagrange(nodes, a, x):
     return y
 
 
-def calculate_lagrange_output(nodes):
+def generate_equation(nodes, degree):
     a = lagrange_coefficients(nodes)
-    y_coordinates = np.array([calculate_lagrange(nodes, a, x) for x in nodes])
+    equation = f"$P_{degree}(x) = {a[0]}"
+    w = [(f"(x - {xi})") for xi in nodes]
+    b = ""
+
+    for i in range(1, len(a)):
+        for j in range(i):
+            b += w[j]
+        equation += f" + {a[i]}{b}"
+        b = ""
+
+    return equation + "$"
+
+
+def calculate_lagrange_output(nodes, x_coords=[]):
+    a = lagrange_coefficients(nodes)
+    if len(x_coords) == 0:
+        x_coords = nodes
+    y_coordinates = np.array([calculate_lagrange(nodes, a, x) for x in x_coords])
 
     return y_coordinates
 
 
 def chebyshev_nodes(n):
     nodes = np.array(
-        [(math.cos((2 * k - 1) * math.pi / (2 * n))) for k in range(1, n+1)]
+        [(math.cos((2 * k - 1) * math.pi / (2 * n))) for k in range(1, n + 1)]
     )
 
     return nodes
 
-def plot_functions(x_sets, y_sets, color_nums, markers, labels, title="", xlabel=r"$x$", ylabel=r"$y$"):
-    # Enable Latex
-    plt.rcParams["text.usetex"] = True
-    # Styling
-    plt.style.use("seaborn-v0_8-deep")
-    plt.rc("grid", color="#a6a6a6", linestyle="dotted", linewidth=0.5)
-    prop_cycle = plt.rcParams['axes.prop_cycle']
-    default_colors = prop_cycle.by_key()['color']
-    
-    total_functions = len(x_sets)
-    fig, ax = plt.subplots()
-    for i in range(total_functions):
-        ax.plot(x_sets[i], y_sets[i], color=default_colors[color_nums[i]], marker=markers[i], label=labels[i])
-
-    ax.set_title(title)
-    ax.set_xlabel(xlabel)
-    ax.set_ylabel(ylabel)
-    ax.legend()
-    return fig
 
 if __name__ == "__main__":
     # Question 2
+    # NOTE: 2.2 and 2.3 are to be executed independently, 
+    # comment out one when executing the other.
+    title = "Equidistant Lagrange Interpolation"
     x = np.linspace(-1, 1, 100)
     y = f(x)
+    fig_f = Figure(x, y, r"$f(x)$")
 
     # Question 2.2 (Equidistant Nodes)
-    # n+1 nodes for P_n
-    five_x = np.linspace(-1, 1, 6)
-    five_y = calculate_lagrange_output(five_x)
+    #-----------------------------------------------------------
+    # nodes_deg_5 = np.linspace(-1, 1, 6)
+    # y_nodes_5 = calculate_lagrange_output(nodes_deg_5)
+    # y_poly_5 = calculate_lagrange_output(nodes_deg_5, x)
+    # f1 = Figure(x, y_poly_5, r"$P_5(x)$", 1)
+    # f2 = Figure(nodes_deg_5, y_nodes_5, "", 1, ".", "")
 
-    ten_x = np.linspace(-1, 1, 11)
-    ten_y = calculate_lagrange_output(ten_x)
+    # nodes_deg_10 = np.linspace(-1, 1, 11)
+    # y_nodes_10 = calculate_lagrange_output(nodes_deg_10)
+    # y_poly_10 = calculate_lagrange_output(nodes_deg_10, x)
+    # f3 = Figure(x, y_poly_10, r"$P_{10}(x)$", 2)
+    # f4 = Figure(nodes_deg_10, y_nodes_10, "", 2, ".", "")
 
-    twenty_x = np.linspace(-1, 1, 21)
-    twenty_y = calculate_lagrange_output(twenty_x)
+    # nodes_deg_20 = np.linspace(-1, 1, 21)
+    # y_nodes_20 = calculate_lagrange_output(nodes_deg_20)
+    # y_poly_20 = calculate_lagrange_output(nodes_deg_20, x)
+    # f5 = Figure(x, y_poly_20, r"$P_{20}(x)$", 3)
+    # f6 = Figure(nodes_deg_20, y_nodes_20, "", 3, ".", "")
+
+    # # Merge and plot figures
+    # fig1 = fig_f.copy().merge([f1, f2], title + " (n = 5)")
+    # fig2 = fig_f.copy().merge([f3, f4], title + " (n = 10)")
+    # fig3 = fig_f.copy().merge([f5, f6], title + " (n = 20)")
+    # fig4 = fig_f.copy().merge([f1, f3, f5])
+    # fig1.get_figure('./plots_2/q2_2/p5.png')
+    # fig2.get_figure('./plots_2/q2_2/p10.png')
+    # fig3.get_figure('./plots_2/q2_2/p20.png')
+    # fig4.get_figure('./plots_2/q2_2/all.png')
+
+    # # Generate and save equations to a text file
+    # equations = [
+    #     generate_equation(nodes_deg_5, 5),
+    #     generate_equation(nodes_deg_10, 10),
+    #     generate_equation(nodes_deg_20, 20),
+    # ]
     
-    x_sets = [x, five_x, ten_x, twenty_x]
-    y_sets = [y, five_y, ten_y, twenty_y]
-    labels = [r"$f(x)$",r"$P_5(x)$",r"$P_{10}(x)$",r"$P_{20}(x)$"]
-    title = 'Equidistant Lagrange Interpolation'
-    
-    fig1 = plot_functions(x_sets[:2], y_sets[:2], [0,1], ['','.'], labels[:2], title+" (n = 5)")
-    fig2 = plot_functions(x_sets[:3:2], y_sets[:3:2], [0,2], ['','.'], labels[:3:2], title+" (n = 10)")
-    fig3 = plot_functions(x_sets[:4:3], y_sets[:4:3], [0,3], ['','.'],labels[:4:3], title+" (n = 20)")
-    fig4 = plot_functions(x_sets, y_sets, [i for i in range(4)], ['','','',''], labels, title)
-    
-    # Save Figures
-    # fig1.savefig('<path>/plots_2/p5.png', dpi = 300)
-    # fig2.savefig('<path>/plots_2/p10.png', dpi = 300)
-    # fig3.savefig('<path>/plots_2/p20.png', dpi = 300)
-    # fig4.savefig('<path>/plots_2/all.png', dpi = 300)
-    
-    plt.show()
-    
+    # with open("/Users/User/Repos/numerical_analysis/assignment_2/plots_2/q2_2/lagrange_equations.txt", "w") as f:
+    #     for eq in equations:
+    #         f.write(eq + "\n")
+
+    # plt.show()
+    #-----------------------------------------------------------
+
     # Question 2.3 (Chebyshev Nodes)
-    five_x = chebyshev_nodes(6)
-    five_y = calculate_lagrange_output(five_x)
+    #-----------------------------------------------------------
+    # title = "Chebyshev Lagrange Interpolation"
 
-    ten_x = chebyshev_nodes(11)
-    ten_y = calculate_lagrange_output(ten_x)
+    # nodes_deg_5 = chebyshev_nodes(6)
+    # y_nodes_5 = calculate_lagrange_output(nodes_deg_5)
+    # y_poly_5 = calculate_lagrange_output(nodes_deg_5, x)
+    # f1 = Figure(x, y_poly_5, r"$P_5(x)$", 1)
+    # f2 = Figure(nodes_deg_5, y_nodes_5, "", 1, ".", "")
 
-    twenty_x = chebyshev_nodes(21)
-    twenty_y = calculate_lagrange_output(twenty_x)
+    # nodes_deg_10 = chebyshev_nodes(11)
+    # y_nodes_10 = calculate_lagrange_output(nodes_deg_10)
+    # y_poly_10 = calculate_lagrange_output(nodes_deg_10, x)
+    # f3 = Figure(x, y_poly_10, r"$P_{10}(x)$", 2)
+    # f4 = Figure(nodes_deg_10, y_nodes_10, "", 2, ".", "")
+
+    # nodes_deg_20 = chebyshev_nodes(21)
+    # y_nodes_20 = calculate_lagrange_output(nodes_deg_20)
+    # y_poly_20 = calculate_lagrange_output(nodes_deg_20, x)
+    # f5 = Figure(x, y_poly_20, r"$P_{20}(x)$", 3)
+    # f6 = Figure(nodes_deg_20, y_nodes_20, "", 3, ".", "")
+
+    # # Merge and plot figures
+    # fig1 = fig_f.copy().merge([f1, f2], title + " (n = 5)")
+    # fig2 = fig_f.copy().merge([f3, f4], title + " (n = 10)")
+    # fig3 = fig_f.copy().merge([f5, f6], title + " (n = 20)")
+    # fig4 = fig_f.copy().merge([f1, f3, f5])
+    # fig1.get_figure('./plots_2/q2_3/chebyshev_p5.png')
+    # fig2.get_figure('./plots_2/q2_3/chebyshev_p10.png')
+    # fig3.get_figure('./plots_2/q2_3/chebyshev_p20.png')
+    # fig4.get_figure('./plots_2/q2_3/chebyshev_all.png')
+
+    # # Generate and save equations to a text file
+    # equations = [
+    #     generate_equation(nodes_deg_5, 5),
+    #     generate_equation(nodes_deg_10, 10),
+    #     generate_equation(nodes_deg_20, 20),
+    # ]
     
-    x_sets = [x, five_x, ten_x, twenty_x]
-    y_sets = [y, five_y, ten_y, twenty_y]
-    labels = [r"$f(x)$",r"$P_5(x)$",r"$P_{10}(x)$",r"$P_{20}(x)$"]
-    title = 'Chebyshev Lagrange Interpolation'
-    
-    fig1 = plot_functions(x_sets[:2], y_sets[:2], [0,1], ['','.'], labels[:2], title+" (n = 5)")
-    fig2 = plot_functions(x_sets[:3:2], y_sets[:3:2], [0,2], ['','.'], labels[:3:2], title+" (n = 10)")
-    fig3 = plot_functions(x_sets[:4:3], y_sets[:4:3], [0,3], ['','.'],labels[:4:3], title+" (n = 20)")
-    fig4 = plot_functions(x_sets, y_sets, [i for i in range(4)], ['','','',''], labels, title)
-    
-    # Save Figures
-    # fig1.savefig('<path>/plots_2/q2_3/chebyshev_p5.png', dpi = 300)
-    # fig2.savefig('<path>/plots_2/q2_3/chebyshev_p10.png', dpi = 300)
-    # fig3.savefig('<path>/plots_2/q2_3/chebyshev_p20.png', dpi = 300)
-    # fig4.savefig('<path>/plots_2/q2_3/chebyshev_all.png', dpi = 300)
-    
-    plt.show()
+    # with open("/Users/User/Repos/numerical_analysis/assignment_2/plots_2/q2_3/lagrange_equations_chebyshev.txt", "w") as f:
+    #     for eq in equations:
+    #         f.write(eq + "\n")
+
+    # plt.show()
+    #-----------------------------------------------------------
